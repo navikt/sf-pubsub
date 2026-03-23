e75b662c38118472a05877cc0a9d5490a9a8bb84
Björn Hägglund <bjorn.hagglund@nav.no>
2026-03-23 15:07:01 +0100

    Log inside witg logging context

--- src/main/kotlin/no/nav/sf/pubsub/pubsub/recordHandler.kt
+++ src/main/kotlin/no/nav/sf/pubsub/pubsub/recordHandler.kt
@@ -107,7 +107,7 @@ fun teamLogsRecordHandler(eventType: EventTypeTeamLog): (GenericRecord) -> Boole
                 ?.takeIf { !it.isJsonNull }
                 ?.asString
 
-            log.info { "teamLogsRecordHandler invoked, levelValue=$levelValue" }
+            log.info ( "teamLogsRecordHandler invoked, levelValue=$levelValue" )
 
             if (eventType.fieldForLogLevelFilter == null ||
                 obj[eventType.fieldForLogLevelFilter].asString == "Error" ||
@@ -115,6 +115,7 @@ fun teamLogsRecordHandler(eventType: EventTypeTeamLog): (GenericRecord) -> Boole
             ) {
                 val logMessage = obj[eventType.messageField]?.asString ?: "N/A"
                 withLoggingContext(loggingContext) {
+                    log.info ( "teamLogsRecordHandler will team log error a record" )
                     log.error(TEAM_LOGS, logMessage)
                 }
             }
