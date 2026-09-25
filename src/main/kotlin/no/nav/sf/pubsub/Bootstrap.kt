d9bdd073977e46bb68b419396c8fafabbbcd6ee8
Sebastian Hoxhallari <sebastian.hoxhallari@nav.no>
2026-09-25 14:14:42 +0200

    Minor fix

--- src/main/kotlin/no/nav/sf/pubsub/Bootstrap.kt
+++ src/main/kotlin/no/nav/sf/pubsub/Bootstrap.kt
@@ -3,9 +3,9 @@ package no.nav.sf.pubsub
 import no.nav.sf.pubsub.logs.EventTypeTeamLog
 import no.nav.sf.pubsub.pubsub.appendToPodFileHandler
 import no.nav.sf.pubsub.pubsub.localRecordHandler
-import no.nav.sf.pubsub.pubsub.setUUIDFromPayloadFieldKafkaRecordHandler
 import no.nav.sf.pubsub.pubsub.puzzelPSRRecordHandler
 import no.nav.sf.pubsub.pubsub.randomUUIDKafkaRecordHandler
+import no.nav.sf.pubsub.pubsub.setUUIDFromPayloadFieldKafkaRecordHandler
 import no.nav.sf.pubsub.pubsub.teamLogsRecordHandler
 import no.nav.sf.pubsub.puzzel.puzzelMappingCache
 
